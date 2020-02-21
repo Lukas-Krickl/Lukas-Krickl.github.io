02b6cf76802af6bd552c43980bfb0754c1419a22
Lukas <lukas.krickl@outlook.at>
2020-02-21 20:55:25 +0100

    portfolio v1.1

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 const navbar = document.getElementById('navbar');
 const navItems = navbar.children;
 const footerArrow = document.getElementById('footer-arrow');
-const pages = document.getElementsByTagName("section");
-var pageHeight = pages[0].offsetHeight -100; //height of each page (sections)
+const tolerance = 100;
+var pages = getPages();
 var pageScrollHeight = getPageScrollHeight();   //starting height of each page(sections) first not included
 var footerChangeHeight = 100;
 
@@ -13,54 +13,71 @@ var enabledPageHeader = 1; //page nr of the enabled header
 const projects =  document.getElementsByClassName("p4-project"); //page 4 projects
 const indicators = document.getElementById('project-indicator').children;
 
-//compute and returns a list of heights where each page starts
-//first not included, would be 0
-function getPageScrollHeight() {
-  pageHeight = pages[0].offsetHeight;
-  let pageScrollHeight = [];
+//retreive sections from document
+function getPages() {
+  let pages = [...document.getElementsByTagName("section")];
+  let removePage = -1; //for removing not displayed sections
   for (var i = 1; i < pages.length; i++) {
-    pageScrollHeight.push(pageHeight*i-100); //40px tolerance to change more rapid
+    if(pages[i].offsetHeight == 0) {
+      removePage = i;
+    }
+  }
+  if (removePage != -1) {
+    pages.splice(removePage , 1);
+  }
+  console.log('amount of sections: ', pages.length);
+  return pages;
+}
+
+//compute and returns a list of heights where each page starts
+function getPageScrollHeight() {
+  let pageScrollHeight = [];
+
+  pageScrollHeight.push(0)
+  for (var i = 1; i < pages.length; i++) {
+    console.log('i=', i, ' page offset ',pages[i].offsetHeight);
+    pageScrollHeight.push(pageScrollHeight[i-1]+pages[i-1].offsetHeight-tolerance); //40px tolerance to change more rapid
   }
   console.log('page scroll height',  pageScrollHeight);
-  pageHeight -= 100;
   return pageScrollHeight;
 }
 
 //on window resized, compute scroll points
 window.addEventListener('resize', () => {
-  if (pageHeight != pages[0].offsetHeight) {  //if section size actually changed
+  if (pageScrollHeight[1] != pages[1].offsetHeight) {  //if section size actually changed
+    pages = getPages();
     pageScrollHeight = getPageScrollHeight();
   }
 });
 
 //event listener for changing navbar class, navbar is triggered on top of page
 window.addEventListener('scroll', () => {
-  if (this.scrollY < pageScrollHeight[0]) {
+  if (this.scrollY < pageScrollHeight[1]) {
     toggleNavActive(1);
 
-  } else if (this.scrollY >= pageScrollHeight[0] && this.scrollY < pageScrollHeight[1]) {
+  } else if (this.scrollY >= pageScrollHeight[1] && this.scrollY < pageScrollHeight[2]) {
     toggleNavActive(2);
 
-  } else if (this.scrollY >= pageScrollHeight[1] && this.scrollY < pageScrollHeight[2]) {
+  } else if (this.scrollY >= pageScrollHeight[2] && this.scrollY < pageScrollHeight[3]) {
     toggleNavActive(3);
 
-  } else if (this.scrollY >= pageScrollHeight[2] && this.scrollY < pageScrollHeight[3]) {
+  } else if (this.scrollY >= pageScrollHeight[3] && this.scrollY < pageScrollHeight[4]) {
     toggleNavActive(4);
 
-  } else if (this.scrollY >= pageScrollHeight[3] && this.scrollY < pageScrollHeight[4]) {
+  } else if (this.scrollY >= pageScrollHeight[4] && this.scrollY < pageScrollHeight[5]) {
     toggleNavActive(5);
 
-  } else if (this.scrollY >= pageScrollHeight[4]) {
+  } else if (this.scrollY >= pageScrollHeight[5]) {
     toggleNavActive(6);
   }
 
   //toggle fisrt page header and other page header
-  if (this.scrollY < pageHeight && navbar.classList.contains('nav-page-other')) {
+  if (this.scrollY < pageScrollHeight[1] && navbar.classList.contains('nav-page-other')) {
     navbar.classList.remove('nav-page-other');
     navbar.classList.add('nav-page-1');
     navItems[0].classList.add('nav-item-active-1');
 
-  } else if (this.scrollY >= pageHeight && navbar.classList.contains('nav-page-1')) {
+  } else if (this.scrollY >= pageScrollHeight[1] && navbar.classList.contains('nav-page-1')) {
     navbar.classList.remove('nav-page-1');
     navbar.classList.add('nav-page-other');
     navItems[0].classList.remove('nav-item-active-1');
