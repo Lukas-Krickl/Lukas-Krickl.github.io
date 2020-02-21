b633a987f6a9a44f92cc19358f2538baa0d537f9
Lukas <lukas.krickl@outlook.at>
2020-02-21 18:56:29 +0100

    portfolio v1.0

---
+++ js/script.js
@@ -0,0 +1,134 @@
+//get elements once, no redundant DOM access
+const navbar = document.getElementById('navbar');
+const navItems = navbar.children;
+const footerArrow = document.getElementById('footer-arrow');
+const pages = document.getElementsByTagName("section");
+var pageHeight = pages[0].offsetHeight -100; //height of each page (sections)
+var pageScrollHeight = getPageScrollHeight();   //starting height of each page(sections) first not included
+var footerChangeHeight = 100;
+
+var enabledPageFooter = 1; //page nr of the enabled header
+var enabledPageHeader = 1; //page nr of the enabled header
+
+const projects =  document.getElementsByClassName("p4-project"); //page 4 projects
+const indicators = document.getElementById('project-indicator').children;
+
+//compute and returns a list of heights where each page starts
+//first not included, would be 0
+function getPageScrollHeight() {
+  pageHeight = pages[0].offsetHeight;
+  let pageScrollHeight = [];
+  for (var i = 1; i < pages.length; i++) {
+    pageScrollHeight.push(pageHeight*i-100); //40px tolerance to change more rapid
+  }
+  console.log('page scroll height',  pageScrollHeight);
+  pageHeight -= 100;
+  return pageScrollHeight;
+}
+
+//on window resized, compute scroll points
+window.addEventListener('resize', () => {
+  if (pageHeight != pages[0].offsetHeight) {  //if section size actually changed
+    pageScrollHeight = getPageScrollHeight();
+  }
+});
+
+//event listener for changing navbar class, navbar is triggered on top of page
+window.addEventListener('scroll', () => {
+  if (this.scrollY < pageScrollHeight[0]) {
+    toggleNavActive(1);
+
+  } else if (this.scrollY >= pageScrollHeight[0] && this.scrollY < pageScrollHeight[1]) {
+    toggleNavActive(2);
+
+  } else if (this.scrollY >= pageScrollHeight[1] && this.scrollY < pageScrollHeight[2]) {
+    toggleNavActive(3);
+
+  } else if (this.scrollY >= pageScrollHeight[2] && this.scrollY < pageScrollHeight[3]) {
+    toggleNavActive(4);
+
+  } else if (this.scrollY >= pageScrollHeight[3] && this.scrollY < pageScrollHeight[4]) {
+    toggleNavActive(5);
+
+  } else if (this.scrollY >= pageScrollHeight[4]) {
+    toggleNavActive(6);
+  }
+
+  //toggle fisrt page header and other page header
+  if (this.scrollY < pageHeight && navbar.classList.contains('nav-page-other')) {
+    navbar.classList.remove('nav-page-other');
+    navbar.classList.add('nav-page-1');
+    navItems[0].classList.add('nav-item-active-1');
+
+  } else if (this.scrollY >= pageHeight && navbar.classList.contains('nav-page-1')) {
+    navbar.classList.remove('nav-page-1');
+    navbar.classList.add('nav-page-other');
+    navItems[0].classList.remove('nav-item-active-1');
+  }
+});
+
+
+function toggleNavActive(pageNr) {
+  //checks if page even changed, prevents unnecessary computation,
+  //leaving more cpu time for other tasks
+  if (pageNr != enabledPageHeader) {
+    //page changed
+    enabledPageHeader = pageNr;
+    resetActiveClass();
+
+    if (pageNr != 1) {  // first page has separat active tag
+        navItems[pageNr-1].classList.add('nav-item-active');
+    }
+  }
+}
+
+function resetActiveClass() {
+  for (var i = 0; i < navItems.length; i++) {
+    navItems[i].classList.remove('nav-item-active')
+  }
+}
+
+//event listener for changing footer, footer is triggered on bottom of page
+// enabledPageFooter prevents changing classes when user is scrolling without changing page
+window.addEventListener('scroll', () => {
+  if (this.scrollY < footerChangeHeight && enabledPageFooter != 1) {
+    toggleFooter(true);
+    enabledPageFooter = 1;
+
+  } else if (this.scrollY > footerChangeHeight && enabledPageFooter == 1) {
+    toggleFooter(false);
+    enabledPageFooter = 2;
+  }
+});
+
+function toggleFooter(enableFirstPageFooter) {
+  if (enableFirstPageFooter) {
+    if (footerArrow.classList.contains('hide')) {
+      footerArrow.classList.remove('hide');
+    }
+  } else {
+    if (!footerArrow.classList.contains('hide')) {
+      footerArrow.classList.add('hide');
+    }
+  }
+}
+
+function cycleProject(forwards) {
+  active = findActiveProject();
+  direction = forwards ? 1:-1;
+  newActive = (active+direction) % projects.length;
+  //disable old
+  projects[active].classList.add('hide');
+  indicators[active].classList.remove('active-indicator');
+
+  //enable new
+  projects[newActive].classList.remove('hide');
+  indicators[newActive].classList.add('active-indicator');
+}
+
+function findActiveProject() {
+  for (var i = 0; i < projects.length; i++) {
+    if(!projects[i].classList.contains('hide'))
+    return i;
+  }
+}
